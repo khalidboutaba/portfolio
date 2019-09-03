2289fd21cafbc1e34c835e5f37bea7dd04c0cda8
khalidboutaba <khalidboutaba1@gmail.com>
2019-09-03 11:16:03 +0100

    add blog app

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'job.apps.JobConfig',
     'blog.apps.BlogConfig',
+    
 
 ]
 
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [''],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

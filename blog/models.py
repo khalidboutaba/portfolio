7a1e3f093ee23325c90f083ccc9ce2ffe194b79a
khalidboutaba <khalidboutaba1@gmail.com>
2019-09-04 10:23:11 +0100

    site complet

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,12 @@ class Blog(models.Model):
     pub_date = models.DateField()
     body = models.TextField(max_length=500)
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%A %d %Y')
+
+    def summary(self):
+        return self.body[:100]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Blog
 
-# Create your views here.
+def blog(request):
+    blogs=Blog.objects.order_by('-pub_date')[2:]
+    latest_blogs=Blog.objects.order_by('-pub_date')[:2]
+    print(blogs)
+    return render(request,'blog/blog.html',{'blogs':blogs, 'latest_blogs':latest_blogs})
+
+def detail(request, id):
+    detailblog = get_object_or_404(Blog, pk=id)
+    return render(request, 'blog/detail.html',{'detailblog':detailblog})

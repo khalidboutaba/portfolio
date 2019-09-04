3021eca30d37064a874285768eaff74e7a42c76e
khalidboutaba <khalidboutaba1@gmail.com>
2019-09-04 15:57:45 +0100

    Blog changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'nettworck',
+        'PASSWORD': 'django1234',
         'HOST': 'localhost',
         'PORT': '5432'
     }
